144115755bb5c4e610b0d32d24be2a59526bc15d
1moonala <junseomoon9@gmail.com>
2021-07-04 20:50:09 -0400

    Added loading Screen Animation

--- src/components/MainApp.js
+++ src/components/MainApp.js
@@ -1,20 +1,58 @@
-import React from 'react'
+import React, {useState, useEffect} from 'react'
 import Header from './Header'
 import Map from "./Map"
 import LeftSidebar from "./LeftSidebar"
 import RightSidebar from './RightSidebar'
+import CircleLoader from "react-spinners/CircleLoader";
 
 const MainApp = () => {
-    return (
-        <div className="mainapp"> 
-            <Header/>
-            <div className="main-app-body">
-                <LeftSidebar/>
-                <Map/>
-                <RightSidebar/>
+
+    let [loading, setLoading] = useState(false);
+    let [animation, setAnimation] = useState(false);
+    let [color, setColor] = useState("#ffffff");
+
+    useEffect(() => {
+        setLoading(true)
+        setTimeout(() => {
+            setLoading(false)
+        }, 3000)
+        setAnimation(true)
+        setTimeout(() => {
+            setAnimation(false)
+        }, 4000)
+    }, [])
+
+    if (loading) {
+        return (
+            <div className="loading-screen">
+                <CircleLoader color={color} />
             </div>
-        </div>
-    )
+        )
+    }
+    else {
+        if (animation) {
+            return (
+                <div className="animation">
+                    <div className="left-container"></div>
+                    <div className="right-container"></div>
+                </div>
+            )
+        }
+        else {  
+            return (
+                <div className="mainapp"> 
+                    <Header/>
+                    <div className="main-app-body">
+                        <LeftSidebar/>
+                        <Map/>
+                        <RightSidebar/>
+                    </div>
+                </div>
+            )
+        }
+        
+    }
+    
 }
 
 export default MainApp
